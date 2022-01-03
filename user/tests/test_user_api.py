c343e72cfd634c9c24769a9cab1e30791712c61c
fateme-zka <fatemezahrakalantarii@gmail.com>
2022-01-03 06:19:42 -0800

    Added manage user endpoint.

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -10,6 +10,8 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 # url to generate the user token
 TOKEN_URL = reverse('user:token')
+# update user endpoint url  user/"me" => the account of the user who is authenticated
+ME_URL = reverse('user:me')
 
 
 # helper function
@@ -17,7 +19,6 @@ def create_user_customized(**params):  # **params is a dynamic list of arguments
     return get_user_model().objects.create_user(**params)
 
 
-# private api is like modifying user or making some change in user.
 # public api is not authenticated (without creating user and logging in)
 class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
@@ -107,3 +108,59 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # ----------------------------------------------------manage user tests
+
+    # the authentication is required for the endpoint (it's an important part)
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+# authentication is required before you can use those points.
+# private api is like modifying user or making some change in user.
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        """Set the Authentication for whole tests in this class"""
+        self.user = create_user_customized(
+            email='test3@hotmail.com',
+            password='testpass2',
+            name='fateme3'
+        )
+        # Just make reusable client
+        self.client = APIClient()
+        # to forcte to authenticate any requests that the client makes with our sample user
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # res does not send the hash password it only sends email and name
+        self.assertEqual(res.data, {
+            'email': self.user.email,
+            'name': self.user.name
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'name': 'new_name', 'password': 'new_password'}
+        res = self.client.patch(ME_URL, payload)
+
+        # a helper function which refresh database by latest values
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- user/views.py
+++ user/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .serializers import AuthTokenSerializer, UserSerializer
 
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings, APISettings
-from rest_framework import generics
+from rest_framework.settings import api_settings
+from rest_framework import generics, authentication, permissions
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -15,3 +15,15 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage (update) the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    # permissions are the level of access that the user has
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
